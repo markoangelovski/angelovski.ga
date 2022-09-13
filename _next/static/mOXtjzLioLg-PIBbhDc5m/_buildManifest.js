695f87e139b74b1324b5d23ac41404de484f7432
markoangelovski <46273540+markoangelovski@users.noreply.github.com>
2022-09-13 23:30:04 +0000

    Deploying to gh-pages from @ markoangelovski/angelovski.ga@9520d3b8c068454a96f96610fade1d6ec9bc7dd3 🚀

--- _next/static/mOXtjzLioLg-PIBbhDc5m/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],"/[[...slug]]":["static/chunks/49-4906ec0499ea7f47.js","static/chunks/pages/[[...slug]]-a86365cfaef624f1.js"],sortedPages:["/_app","/_error","/[[...slug]]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
